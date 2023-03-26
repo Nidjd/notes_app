45a79f1be164562d281f9e3f89a40103f2db577e
Mohammed Almahmoud <malmhmwd238@gmail.com>
2023-03-26 04:20:04 -0700

    the them is ready and build the appbar

---
+++ lib/views/widgets/custom_search_icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomIconSearch extends StatelessWidget {
+  const CustomIconSearch({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      width: 46,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.05),
+      ),
+      child: const Icon(
+        Icons.search,
+        size: 28,
+      ),
+    );
+  }
+}
