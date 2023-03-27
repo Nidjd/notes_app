ea7b1e5864210d85a671c083e3980c5388e8d880
Mohammed Almahmoud <malmhmwd238@gmail.com>
2023-03-27 00:57:02 -0700

    add floating action button

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -4,6 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'custom_app_bar.dart';
 import 'custom_note_item.dart';
+import 'notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
