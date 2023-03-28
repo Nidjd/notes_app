f738dd8e02f456f915ac5a3bda1090b8575f3d21
Mohammed Almahmoud <malmhmwd238@gmail.com>
2023-03-28 03:16:29 -0700

    create type adapter for node model

--- lib/models/node_model.dart
+++ lib/models/node_model.dart
@@ -1,7 +1,16 @@
-class NodeModel {
+import 'package:hive/hive.dart';
+
+part 'node_model.g.dart';
+
+@HiveType(typeId: 0)
+class NodeModel extends HiveObject{
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NodeModel({required this.title,required this.subTitle,required this.date,required this.color});
